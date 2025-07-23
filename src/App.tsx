f328e1dc7b0fa075f3f42226e66a0a89dbddaf2f
Kezia Santnn <keziasantnn@gmail.com>
2025-07-23 18:48:02 -0300

    estilização principal da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTelegram,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { BsEnvelopeAt } from "react-icons/bs";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +19,39 @@ function App() {
         <a>Eventos</a>
         <a>Contatos</a>
       </nav>
+      <main>
+        <section className="principal">
+          <div className="principal-imgs">
+            <img
+              className="perfil"
+              src="assets/MARTALUNA.jpg"
+              alt="foto-perfil"
+              height="300px"
+            />
+            <img className="logo" src="assets/CAPA.png" alt="logo-page" />
+          </div>
+          <div className="principal-buttons">
+            <div>
+              <a className="social-item">
+                <FaFacebook />
+              </a>
+              <a className="social-item">
+                <FaTelegram />
+              </a>
+              <a className="social-item">
+                <FaInstagram />
+              </a>
+              <a className="social-item">
+                <BsEnvelopeAt />
+              </a>
+              <a className="social-item">
+                <FaWhatsapp />
+              </a>
+            </div>
+            <button className="agenda">Agendar Consulta</button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
